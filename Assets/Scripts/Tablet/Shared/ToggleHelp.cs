1148bf098b705f2e62f6c69d2cda73d2898dbfac
Maxime Druart <maxime41010.druart@gmail.com>
2022-06-08 09:40:09 +0200

    sidebar WIP

--- Assets/Scripts/Tablet/Shared/ToggleHelp.cs
+++ Assets/Scripts/Tablet/Shared/ToggleHelp.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ToggleHelp : MonoBehaviour
 {
 
     private bool isOpened = false;
+
+    public GameObject Sidebar;
+    private RectTransform SidebarRect;
+
+    private Sequence mySequence;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SidebarRect = Sidebar.GetComponent<RectTransform>();
+        mySequence = DOTween.Sequence();
+        mySequence.Append(SidebarRect.DOLocalMoveX(120, 0.3f));
+        mySequence.Pause();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void Toggle(bool _) {
+    public void Toggle(bool _)
+    {
         isOpened = !isOpened;
+        if (!isOpened)
+        {
+            Debug.Log("playing !!");
+            mySequence.Play();
+        }
+        else
+        {
+            mySequence.PlayBackwards();
+        }
     }
 }
